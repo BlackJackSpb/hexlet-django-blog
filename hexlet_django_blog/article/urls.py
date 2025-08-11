93eb2d1db1639ac59e563dde9ba12801bc21df10
BlackJackSPB <Rystzov1995@yandex.ru>
2025-08-11 18:55:26 +0300

    Обновление CRUD

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,6 +1,11 @@
 # hexlet_django_blog/article/urls.py
 from django.urls import path
-from .views import IndexView, ArticleView, ArticleFormCreateView
+from .views import (
+IndexView,
+ArticleView,
+ArticleFormCreateView,
+ArticleFormEditView
+)
 
 
 app_name = "article"
@@ -9,5 +14,6 @@ app_name = "article"
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
     path('<int:id>/', ArticleView.as_view(), name='show'),
+    path("<int:id>/edit/", ArticleFormEditView.as_view(), name="update"),
     path('create/', ArticleFormCreateView.as_view(), name='create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -35,7 +35,32 @@ class ArticleFormCreateView(View):
         form = ArticleForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Статья успешно создана!')  # ← флеш-сообщение
+            messages.success(request, 'Статья успешно создана!')
             return redirect('article:index')
-        # Если форма невалидна — показываем её снова с ошибками
         return render(request, 'articles/create.html', {'form': form})
+
+
+class ArticleFormEditView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs['id']
+        article = get_object_or_404(Article, id=article_id)
+        form = ArticleForm(instance=article)  # ← данные из модели
+        return render(
+            request,
+            'articles/update.html',
+            {'form': form, 'article_id': article_id}
+        )
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs['id']
+        article = get_object_or_404(Article, id=article_id)
+        form = ArticleForm(request.POST, instance=article)  # ← обновляем существующую
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Статья успешно обновлена!')
+            return redirect('article:index')
+        return render(
+            request,
+            'articles/update.html',
+            {'form': form, 'article_id': article_id}
+        )
